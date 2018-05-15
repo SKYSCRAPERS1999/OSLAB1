b3798405245c6e03b073f95c1f603d06111fec72
tracer <tracer@tracer.org>
2018-05-15 13:52:39 +0800

    > build lanfangzhou Linux impulse 4.13.0-32-generic #35~16.04.1-Ubuntu SMP Thu Jan 25 10:13:43 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

--- src/pmm.c
+++ src/pmm.c
@@ -1,4 +1,36 @@
 #include <os.h>
+#include <mylib.h>
 
 MOD_DEF(pmm) {
+	.init = pmm.init,
+	.alloc = pmm.alloc,
+	.free = pmm.free,
 };
+
+typedef struct node{
+    void* addr;
+    size_t size;
+    int freed;
+} mem_dict;
+mem_dict dict[100000];
+static int dict_cnt;
+
+static void* init(){
+	dict_cnt = 1;
+	dict[0] = {_heap.start, _heap.end - heap.start, 1};
+}
+
+static void* alloc(size_t size){
+	int align = 1;
+	while (align < size) align <<= 1;
+	for (int i = 0; i < dict_cnt; i++){
+		if (dict[i].freed && dict[i].addr >= size){
+				
+		}
+	}
+}
+
+static void* free(void* ptr){
+
+
+}
