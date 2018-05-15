286468216de1421f693911d5714c3595b639d41e
tracer <tracer@tracer.org>
2018-05-15 18:41:39 +0800

    > run lanfangzhou Linux impulse 4.13.0-32-generic #35~16.04.1-Ubuntu SMP Thu Jan 25 10:13:43 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

--- src/pmm.c
+++ src/pmm.c
@@ -28,7 +28,7 @@ static void pmm_init(){
 }
 
 static void* pmm_alloc(size_t size){
-	int bitcnt = 1;
+	int bitcnt = 1, align;
 	for (int i = 1; i <= size; i <<= 1) bitcnt++;
 	align = 0x7fffffff ^ ((1 << bitcnt) - 1);
 	for (int i = 0; i < free_cnt; i++){
