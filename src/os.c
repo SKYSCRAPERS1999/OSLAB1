7d6da7a372d350de149220417ebeac0630f4193f
161220059-Lan Fang Zhou <386333667@qq.com>
2018-05-17 01:05:14 +0800

    delete test_mem

--- src/os.c
+++ src/os.c
@@ -20,7 +20,6 @@ static void os_init() {
 }
 
 static void test_mem(){
-  printf("Hello, OS World!\n");
   for (volatile int sz = 1e5; sz > 0; sz-=1e3){
     void *ptr = pmm->alloc(sz);
     printf("0x%x\n", (int)ptr);
