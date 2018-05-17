4a0cdc6ff4c58ee36e3f870d42d32a71d621fc05
tracer <tracer@tracer.org>
2018-05-17 11:24:53 +0800

    > run lanfangzhou Linux impulse 4.13.0-32-generic #35~16.04.1-Ubuntu SMP Thu Jan 25 10:13:43 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

--- include/os.h
+++ include/os.h
@@ -12,8 +12,8 @@
 #endif
 
 #ifdef __LOCAL_TEST__
-# define _test(fmt, ...) printf("%s: " fmt "\033[0m", \
-    __func__, ##__VA_ARGS__)
+# define _test(fmt, ...) printf("fmt", \
+    ##__VA_ARGS__)
 #else
 # define _test(...) ((void) 0)
 #endif
