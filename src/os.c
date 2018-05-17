a088c17a6f64c5bf0ab40f5fd8f68e8f4e6e9147
tracer <tracer@tracer.org>
2018-05-17 11:05:01 +0800

    > run lanfangzhou Linux impulse 4.13.0-32-generic #35~16.04.1-Ubuntu SMP Thu Jan 25 10:13:43 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

--- src/os.c
+++ src/os.c
@@ -21,7 +21,6 @@ static void os_init() {
 
 static void f(void* arg) {
   while (1) {
-    for (volatile int i = 0; i < 10000000; i++);
     for (volatile int i = 0; i < 20; i++) printf("%c%c", arg, "\0\n"[i==20-1]);
   }
 }
