91ccf4e343fddf779d3ebbc112483fe16b6e6826
tracer <tracer@tracer.org>
2018-05-17 11:57:17 +0800

    > run lanfangzhou Linux impulse 4.13.0-32-generic #35~16.04.1-Ubuntu SMP Thu Jan 25 10:13:43 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

--- src/os.c
+++ src/os.c
@@ -57,9 +57,9 @@ static _RegSet *os_interrupt(_Event ev, _RegSet *regs) {
   if (t != NULL) thread_id = t->id;
 
   if (ev.event == _EVENT_IRQ_TIMER) {
-    #ifndef OSTEST
+    //#ifndef OSTEST
       _putc('*');
-    #endif
+    //#endif
   }
   if (ev.event == _EVENT_IRQ_IODEV){
     #ifndef OSTEST
