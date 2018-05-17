88991949c444571716b971faf13f6207dca767a3
tracer <tracer@tracer.org>
2018-05-17 12:17:48 +0800

    > run lanfangzhou Linux impulse 4.13.0-32-generic #35~16.04.1-Ubuntu SMP Thu Jan 25 10:13:43 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

--- include/mylib.h
+++ include/mylib.h
@@ -20,10 +20,8 @@ void srand(int);
 unsigned int rand();
 
 uint32_t uptime(); // 系统启动的时间(ms)
-//int read_key(); // 读取一个按键
-//void draw_rect(uint32_t *pixels, int x, int y, int w, int h); // 在屏幕上绘制
-//void draw_sync(); // 同步屏幕
-//int screen_width(); // 获得屏幕的宽度
-//int screen_height(); // 获得屏幕的高度
-
-//void start_game();
+int read_key(); // 读取一个按键
+void draw_rect(uint32_t *pixels, int x, int y, int w, int h); // 在屏幕上绘制
+void draw_sync(); // 同步屏幕
+int screen_width(); // 获得屏幕的宽度
+int screen_height(); // 获得屏幕的高度
