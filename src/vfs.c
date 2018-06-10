1ba94d891706b22021959fd7acb6d2a077f8b056
161220059-Lan Fang Zhou <386333667@qq.com>
2018-06-10 11:39:59 +0800

    add mod-def

---
+++ src/vfs.c
@@ -0,0 +1,23 @@
+#include <os.h>
+#include <mylib.h>
+
+static void vfs_init();
+static int vfs_access(const char *, int);
+static int vfs_unmount(const char *path);
+static int vfs_open(const char *, int);
+static ssize_t vfs_read(int, void *, size_t);
+static ssize_t vfs_write(int, void *, size_t);
+static off_t vfs_lseek(int , off_t offset, int);
+static int vfs_close(int);
+
+MOD_DEF(vfs){
+	.init = vfs_init,
+	.access = vfs_access,
+	.mount = vfs_mount,
+	.unmount = vfs_unmount,
+	.open = vfs_open,
+	.read = vfs_read,
+	.write = vfs_write,
+	.lseek = vfs_lseek,
+	.close = vfs_close,
+};
