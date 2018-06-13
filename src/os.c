fc027bf5bade08ab9a7f84792a9210c502a817f6
tracer <tracer@tracer.org>
2018-06-13 16:26:15 +0800

    > build lanfangzhou Linux impulse 4.13.0-32-generic #35~16.04.1-Ubuntu SMP Thu Jan 25 10:13:43 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

--- src/os.c
+++ src/os.c
@@ -75,7 +75,7 @@ int fd[256];
 static void simple_test(){
   char pool[] = "abcdefghijklmnopqrstuvwxyz";
   for (int i = 0, ret, j; i < 100; i++){
-    strcpy(wbuf, "Hello World "); j = strlen(wbuf); wbuf[j] = pool[i/26]; wbuf[j+1] = '\0'; 
+    strcpy(wbuf, "Hello World "); j = strlen(wbuf); wbuf[j] = pool[i%26]; wbuf[j+1] = '\0'; 
     strcpy(dir, "/Document/"); j = strlen(dir); dir[j] = pool[i%26]; dir[j+1] = pool[i/26]; dir[j+2] = '\0'; 
 
     fd[i] = vfs->open(dir, O_RDWR);
