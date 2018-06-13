82b85e9065fefdc3f88c61b86b8fb1eeabe1218a
tracer <tracer@tracer.org>
2018-06-13 15:22:17 +0800

    > build lanfangzhou Linux impulse 4.13.0-32-generic #35~16.04.1-Ubuntu SMP Thu Jan 25 10:13:43 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

--- src/vfs.c
+++ src/vfs.c
@@ -50,13 +50,13 @@ static inode_t *fsops_lookup(struct filesystem *fs, const char *path, int mode){
 	for (int i = 0; i < NINODES; i++){
 		if (fs->inode[i] == NULL){
 
-			_debug("allocate an inode %d", i);
 
 			fs->inode[i] = (inode_t *)pmm->alloc(sizeof(inode_t));
 			if (NULL == fs->inode[i]) _debug("fs inode allocation failed");
 
 			strcpy(fs->inode[i]->name, path);
 
+			_debug("allocate an inode %d", i);
 			_debug("allocated path is %s", fs->inode[i]->name);
 			
 			fs->inode[i]->ref = 1;
