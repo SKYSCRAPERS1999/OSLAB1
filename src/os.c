82367766b88af23c845d3a926a3a1e18c76ccf1e
tracer <tracer@tracer.org>
2018-06-13 16:31:31 +0800

    > build lanfangzhou Linux impulse 4.13.0-32-generic #35~16.04.1-Ubuntu SMP Thu Jan 25 10:13:43 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

--- src/os.c
+++ src/os.c
@@ -72,7 +72,7 @@ int fd[256];
 
 static void simple_test(){
   char pool[] = "abcdefghijklmnopqrstuvwxyz";
-  for (int i = 0, ret, j; i < 100; i++){
+  for (int i = 0, ret, j; i < 10; i++){
     strcpy(wbuf, "Hello World "); j = strlen(wbuf); wbuf[j] = pool[i%26]; wbuf[j+1] = '\0'; 
     strcpy(dir, "/Document/"); j = strlen(dir); dir[j] = pool[i%26]; dir[j+1] = pool[i/26]; dir[j+2] = '\0'; 
 
@@ -84,10 +84,10 @@ static void simple_test(){
     printf("rsiz = %d\n", ret);
     printf("%s\n\n", rbuf); 
   }
-  for (int i = 0; i < 100; i++) {
+  for (int i = 0; i < 10; i++) {
     vfs->close(fd[i]);
   }
-  for (int i = 100-1, ret, j; i >= 0; i++){
+  for (int i = 10-1, ret, j; i >= 0; i++){
     strcpy(dir, "/Document/"); j = strlen(dir); dir[j] = pool[i%26]; dir[j+1] = pool[i/26]; dir[j+2] = '\0'; 
     fd[i] = vfs->open(dir, O_RDWR);
     vfs->lseek(fd[i], 0, 0);
@@ -95,7 +95,7 @@ static void simple_test(){
     printf("rsiz = %d\n", ret);
     printf("%s\n\n", rbuf); 
   }
-  for (int i = 0; i < 100; i++) {
+  for (int i = 0; i < 10; i++) {
     vfs->close(fd[i]);
   }
 }
