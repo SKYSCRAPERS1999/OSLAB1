5f31fad202a932054891330c214813ce08477ab6
tracer <tracer@tracer.org>
2018-06-13 15:49:52 +0800

    > build lanfangzhou Linux impulse 4.13.0-32-generic #35~16.04.1-Ubuntu SMP Thu Jan 25 10:13:43 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

--- src/vfs.c
+++ src/vfs.c
@@ -127,6 +127,7 @@ static int fileops_open(inode_t *inode, file_t *file, int mode){
 	file->fd = find_nfd(); //allocate an fd.
 
 	if (file->fd == -1) _debug("No available fd!");
+	_debug("file->fd = %d", fd);
 
 	ftable[file->fd] = file;
 	if (ftable[file->fd] == NULL) panic("NULL error");
